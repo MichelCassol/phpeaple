2ca1b321a9e2f518e542a41936d1662697034d72
Michel Oliveira <michelcssoliveira@gmail.com>
2024-07-01 23:34:50 -0300

    Models
    Adicionada a classe de usuário.

---
+++ models/Usuario.php
@@ -0,0 +1,82 @@
+<?php 
+
+class Usuario 
+{
+    private int $id;
+    private string $nome;
+    private string $email;
+    private DateTime $nascimento;
+    private string $senha;
+    private string $caminhoFoto;
+    private string $descricao;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getNascimento()
+    {
+        return $this->nascimento;
+    }
+
+    public function setNascimento($nascimento)
+    {
+        $this->nascimento = $nascimento;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
+
+    public function getCaminhoFoto()
+    {
+        return $this->caminhoFoto;
+    }
+
+    public function setCaminhoFoto($caminhoFoto)
+    {
+        $this->caminhoFoto = $caminhoFoto;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+}
