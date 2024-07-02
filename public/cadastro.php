ea25ac5fa196889f15cc2254ce690ea354b52cfd
Michel Oliveira <michelcssoliveira@gmail.com>
2024-07-01 22:38:47 -0300

    Telas
    Finalizadas as telas de login e cadastro

--- public/cadastro.php
+++ public/cadastro.php
@@ -1,57 +1,58 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de Cadastro</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <style>
-        .hidden {
-            display: none;
-        }
-    </style>
+    <title>Cadastro</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="css/style.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
-    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-2xl flex">
-        <div class="w-3/4 pr-8">
-            <h2 class="text-2xl font-bold mb-6 text-center">Cadastro</h2>
-            <form id="registration-form">
+    <form id="registration-form" class="w-1/3">
+        <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-2xl flex">
+            <div class="w-3/4 pr-8">
+                <h2 class="text-2xl font-bold mb-6 text-center">Cadastro</h2>
                 <div class="mb-4">
-                    <label for="name" class="block text-gray-700">Nome:</label>
-                    <input type="text" id="name" name="name" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                    <label for="nome" class="block text-gray-700">Nome:</label>
+                    <input type="text" id="nome" name="nome" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                 </div>
                 <div class="mb-4">
-                    <label for="dob" class="block text-gray-700">Data de Nascimento:</label>
-                    <input type="date" id="dob" name="dob" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
-                </div>
-                <div class="mb-4">
-                    <label for="profile-picture" class="block text-gray-700">Foto de Perfil:</label>
-                    <input type="file" id="profile-picture" name="profile-picture" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" accept="image/*" onchange="previewImage(event)">
+                    <label for="data-nascimento" class="block text-gray-700">Data de Nascimento:</label>
+                    <input type="date" id="data-nascimento" name="data-nascimento" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                 </div>
                 <div class="mb-4">
                     <label for="email" class="block text-gray-700">Email:</label>
                     <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                 </div>
+                <div class="mb-4">
+                    <label for="senha" class="block text-gray-700">Senha:</label>
+                    <input type="password" id="senha" name="senha" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                </div>
                 <div class="flex items-center justify-between mb-6">
                     <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Cadastrar</button>
-                    <button type="button" class="bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50">Cancelar</button>
+                    <button type="button" onclick="window.location.href='/'" class="bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50">Cancelar</button>
                 </div>
-            </form>
+            </div>
+            <div class="w-1/4 flex row items-center justify-center">
+                <div class="gap-4 flex flex-col justify-center items-center ">
+                    <h2 class="flex text-gray-700 font-bold">Foto de Perfil</h2>
+                    <img id="foto-visual" class="rounded-full border border-gray-300 shadow-lg w-32 h-32 object-cover">
+                    <label for="foto-perfil" class="custom-file-upload">
+                        Carregar Imagem
+                    </label>
+                    <input type="file" id="foto-perfil" name="foto-perfil" class="hidden" accept="image/*" onchange="previewImage(event)">
+                </div>
+            </div>
         </div>
-        <div class="w-1/4 flex items-center justify-center">
-            <img id="profile-preview" src="" alt="Foto de Perfil" class="hidden rounded-full border border-gray-300 shadow-lg w-32 h-32 object-cover">
-        </div>
-    </div>
-    <script>
-        function previewImage(event) {
-            const reader = new FileReader();
-            reader.onload = function(){
-                const preview = document.getElementById('profile-preview');
-                preview.src = reader.result;
-                preview.classList.remove('hidden');
-            }
-            reader.readAsDataURL(event.target.files[0]);
-        }
-    </script>
+    </form>
 </body>
 </html>
+
+<script>
+    function previewImage(event) {
+        const reader = new FileReader();
+        reader.onload = function(){
+            const preview = document.getElementById('foto-visual');
+            preview.src = reader.result;
+        }
+        reader.readAsDataURL(event.target.files[0]);
+    }
+</script>
--- public/login.php
+++ public/login.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entrar</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
@@ -15,12 +13,15 @@
                 <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-4">
-                <label for="password" class="block text-gray-700">Senha:</label>
-                <input type="password" id="password" name="password" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="senha" class="block text-gray-700">Senha:</label>
+                <input type="password" id="senha" name="senha" class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
-            <div class="flex items-center justify-between mb-6">
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Logar</button>
-                <a href="#" class="text-blue-500 hover:underline">Cadastro</a>
+            <div class="flex items-center justify-between mb-6 mt-8">
+                <button type="submit" class="bg-blue-500 w-full text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Logar</button>
+            </div>
+            <div class="flex items-center justify-between mb-2.5">
+                <label for="cadastro" class="text-gray-700">Ainda não possui uma conta? </label>
+                <button id="cadastro" type="button" onclick="window.location.href='public/cadastro.php'" class="text-blue-500 hover:underline">Cadastre-se</button>
             </div>
         </form>
     </div>
