dedc1514ba16739cb4094df9d0d5663fda277eb0
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-07-09 14:05:47 -0300

    Feed
    Configurado o acesso aos perfis no feed de postagens.

--- public/feed.php
+++ public/feed.php
@@ -22,14 +22,14 @@ $total_posts = $postagemController->todasPostagens();
                 <button class="bg-white text-blue-500 px-4 py-2 rounded-lg hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" onclick="window.location.href='/public/perfil.php'">Perfil</button>
                 <h2 class="text-lg text-white">Olá <?=$_SESSION['usuario']?>!</h2>
             </div>
-            <button class="bg-red-500 text-white px-2 py-1 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 mr-4"onclick="window.location.href='../config/logout.php'">Sair</button>
+            <button class="bg-red-500 text-white px-2 py-1 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 mr-4" onclick="window.location.href='../config/logout.php'">Sair</button>
         </div>
     </nav>
     <div class="container xl:w-1/3 lg:w-3/5 sm:w-full mx-auto px-6 py-8 mt-16">
         <?php foreach ($total_posts as $post) : ?>
             <div class="bg-white rounded-lg shadow-lg mb-8">
                 <div class="p-6 grid gap-4 grid-cols-1">
-                    <h2 class="text-lg font-semibold"><?=$post['nome']?></h2>
+                    <button class="flex items-center text-blue-500 hover:text-blue-600 focus:outline-none" onclick="window.location.href='/public/perfil.php'"><?=$post['nome']?></button>
                     <p class="text-gray-700"><?=$post['texto']?></p>
                     <?php if ($post['imagem_arquivo']) : ?>
                         <img src="..<?=$post['imagem_arquivo']?>" alt="postagem" class="w-full rounded-lg ">
